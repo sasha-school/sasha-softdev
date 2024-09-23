c0888bb05dd8392d768f88076c63ab48dc6fbf41
Sasha Murokh <smurokh50@stuy.edu>
2024-09-23 11:43:36 -0400

    updating

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -5,19 +5,26 @@ disco:
 qcc:
 
 how this script works:
+so far, this script only reads and processes the csv and populates a dictionary accordingly,
+accounting for keys with quotes+commas and removing the heading and totals
 
 '''
-
+import pprint
+import random
 occupations = {}
 
 with open('occupations.csv') as text:
     reader = text.read()
     reader = reader.split('\n')
-    for row in range(len(reader)-1):
+    for row in range(len(reader)-2):
         if '"' in reader[row+1]:
             reader[row+1] = reader[row+1].split('",')
+            reader[row+1][0] = reader[row+1][0][1:]
         else:
             reader[row+1] = reader[row+1].split(',')
         occupations[reader[row+1][0]]=float(reader[row+1][1])
-    print(occupations)    
+    pprint.pp(occupations)
+    generated = random.random()*99.8
+    for key in occupations.keys():
+        
     
--- 08_teardown/app.py
+++ 08_teardown/app.py
@@ -5,12 +5,12 @@ DISCO:
 <note any discoveries you made here... no m-atter how small!>
 
 QCC:
-0. 
-1. 
-2. 
-3. 
-4. 
-5. 
+0. without activating the virtual environment, the code will not run because flask is not a built in python3 module
+1. c is an @ suppresor
+2. when it is just run, there is an error: "WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead."
+3. when link is clicked, empty web[page with "no hablo queso"
+4. must be run in virtual environment
+5. how to include user input in such a program?
  ...
 
 INVESTIGATIVE APPROACH:
@@ -22,11 +22,11 @@ INVESTIGATIVE APPROACH:
 
 from flask import Flask
 
-app = Flask(__name__)                    # Q0: Where have you seen similar syntax in other langs? 
+app = Flask(__name__)                    # Q0: Where have you seen similar syntax in other langs? c has the @ symbol that suppresses prints
 
-@app.route("/")                          # Q1: What points of reference do you have for meaning of '/'?
+@app.route("/")                          # Q1: What points of reference do you have for meaning of '/'? directory navigation
 def hello_world():
-    print(__name__)                      # Q2: Where will this print to? Q3: What will it print?
+    print(__name__)                      # Q2: Where will this print to? Q3: What will it print? "No hablo queso!" to the terminal
     return "No hablo queso!"             # Q4: Will this appear anywhere? How u know?
 
 app.run()                                # Q5: Where have you seen similar constructs in other languages?
