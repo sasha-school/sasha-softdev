8e3ea8a0baf488752aee6ade371ec9ccb5d787b5
Sasha Murokh <smurokh50@stuy.edu>
2024-10-08 11:43:49 -0400

    classwork oct 8

---
+++ 15_flask-forms/app.py
@@ -0,0 +1,47 @@
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/", methods=['GET', 'POST'])
+def disp_loginpage():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+#     print("***DIAG: request.args['username']  ***")
+#     print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/auth", methods=['GET', 'POST'])
+def authenticate():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+#     print("***DIAG: request.args['username']  ***")
+#     print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    method1 = request.method
+    username1 = ""#request.form.get['username']
+    
+    return render_template( 'response.html', user=username1, method=method1 )  #response to a form submission
+
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
